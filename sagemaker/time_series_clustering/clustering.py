0e9fb254180acbf7e93abfa6249464bff6076904
ghazwan namou jablak <ghazwan.namou_jablak@syngenta.com>
2023-12-13 17:20:24 +0000

    testing

--- sagemaker/time_series_clustering/clustering.py
+++ sagemaker/time_series_clustering/clustering.py
@@ -8,3 +8,4 @@ if __name__=='__main__':
     file='../../data/Parquet_files/id_field_213.parquet'
     df, field=time_series_processing(file, vi='NDVI',min_date='03-01',max_date='10-31',years=[2017,2018,2019,2020,2021,2022])
     df_metric=dtw_metric(df,field,ref_file_path='../../data/csv_files/NDVI_reference_files/corn_reference.csv', ref_name='Reference')
+    print('end')
